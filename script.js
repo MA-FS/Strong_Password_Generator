ecf025f2dae06fba61d47e5e528e058e203add19
MA-FS <87353040+MA-FS@users.noreply.github.com>
2021-09-06 17:32:03 +0800

    Scripting start, arrays built

--- script.js
+++ script.js
@@ -1,3 +1,26 @@
+// Create arrays for lowercase, uppercase, numeric, special characters
+
+// Lowercase character array
+
+const lowerCaseCharacters = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+
+// Uppercase character array
+
+const upperCaseCharacters = lowerCaseCharacters.map(lowerCaseCharacters => lowerCaseCharacters.toUpperCase());
+
+// Numeric character array
+
+const  numericCharacters = ["1","2","3","4","5","6","7","8","9","0"];
+
+// Special character array
+
+const specialCharacters = [" ","!","#","$","%","&","'","(",")","*","+",",","-",".","/",":",";","<","=",">","?","@","[","\\","]","^","_","`","{","|","}","~"]
+
+// Request password requirements from user
+
+
+
+
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
