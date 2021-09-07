f479ca5c5a68604f2a07d9dfa2194132d5c1f910
MA-FS <87353040+MA-FS@users.noreply.github.com>
2021-09-07 15:16:37 +0800

    Compiled javascript for working password generator

--- script.js
+++ script.js
@@ -1,25 +1,110 @@
-// Create arrays for lowercase, uppercase, numeric, special characters
+// Use funtion to create arrays for lowercase, uppercase, numeric, special characters
 
-// Lowercase character array
+// Align character sets with ASCII table character numbers
+const lowerCaseCharCodes = arrayFromLowToHigh(97, 122)
+const upperCaseCharCodes = arrayFromLowToHigh(65, 90)
+const numericCharCodes = arrayFromLowToHigh(48, 57)
+const specialCharCodes = arrayFromLowToHigh(33, 47).concat(
+  arrayFromLowToHigh(58, 64)
+).concat(
+  arrayFromLowToHigh(91, 96)
+).concat(
+  arrayFromLowToHigh(123, 126)
+)
 
-const lowerCaseCharacters = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+// Function to create arrays from character sets
+function arrayFromLowToHigh(low, high) {
+  const array = []
+  for (let i = low; i <= high; i++){
+    array.push(i)
+  }
+  return array
+}
 
-// Uppercase character array
+// Function for user prompt options
+function getPasswordOptions() {
+  // Password length
+  var length = parseInt(prompt('Select a password length between 8 and 128 characters'), 10);
+  
+  // Check to see if input was a number
+  if (Number.isNaN(length)) {
+    alert('Password must be a number');
+    return null;
+  }
 
-const upperCaseCharacters = lowerCaseCharacters.map(lowerCaseCharacters => lowerCaseCharacters.toUpperCase());
+  // Check to see if number is at least 8 characters long
+  if (length < 8) {
+    alert('Password must be at least 8 characters long');
+    return null;
+  }
+  
+  // Check to see if number is less than 128 characters long
+  if (length > 128) {
+    alert('Password must be 128 characters or less');
+    return null;
+  }
 
-// Numeric character array
+  // Use confirm function to check each of the required criteria
+  // Confirm lowercase characters
+  var hasLowerCaseCharacters = confirm('Click OK to confirm if you would like to include lowercase characters');
 
-const  numericCharacters = ["1","2","3","4","5","6","7","8","9","0"];
+  // Confirm uppercase characters
+  var hasUpperCaseCharacters = confirm('Click OK to confirm if you would like to include uppercase characters');
 
-// Special character array
+  // Confirm numeric characters
+  var hasNumericCharacters = confirm('Click OK to confirm if you would like to include numeric characters');
+  
+  // Confirm special characters
+  var hasSpecialCharacters = confirm('Click OK to confirm if you would like to include special characters');  
 
-const specialCharacters = [" ","!","#","$","%","&","'","(",")","*","+",",","-",".","/",":",";","<","=",">","?","@","[","\\","]","^","_","`","{","|","}","~"]
+  // Check to see if at least one of the options was selected
+  if (
+    hasLowerCaseCharacters === false &&
+    hasUpperCaseCharacters === false &&
+    hasNumericCharacters === false &&
+    hasSpecialCharacters === false
+  ) {
+    alert('At least one option must be selected');
+    return null;
+  }
 
-// Request password requirements from user
+  // Store and return the password options
+  var passwordOptions = {
+    length: length,
+    hasLowerCaseCharacters: hasLowerCaseCharacters,
+    hasUpperCaseCharacters: hasUpperCaseCharacters,
+    hasNumericCharacters: hasNumericCharacters,
+    hasSpecialCharacters: hasSpecialCharacters,
+  };
 
+  return passwordOptions;
+}
 
+// Function to generate password depending on user options
+function generatePassword() {
+  var options = getPasswordOptions();
 
+  // Check if options exists and exit if not to prevent console log errors
+  if (!options) 
+  return null;
+  
+  // Create an array that contains possible character codes depending on user selection
+  let possibleCharacters = [];
+  if (options.hasLowerCaseCharacters) possibleCharacters = possibleCharacters.concat(lowerCaseCharCodes)
+  if (options.hasUpperCaseCharacters) possibleCharacters = possibleCharacters.concat(upperCaseCharCodes)
+  if (options.hasNumericCharacters) possibleCharacters = possibleCharacters.concat(numericCharCodes)
+  if (options.hasSpecialCharacters) possibleCharacters = possibleCharacters.concat(specialCharCodes)
+    
+  // Choose random character codes from the list of possible characters and convert them to their relevent ASCII characters for the user length required
+  const passwordCharacters = []
+  for (let i = 0; i < options.length; i++) {
+    const characterCode = possibleCharacters[Math.floor(Math.random() * possibleCharacters.length)]
+    passwordCharacters.push(String.fromCharCode(characterCode))
+  }
+  
+  return passwordCharacters.join('')
+    
+}
 
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
